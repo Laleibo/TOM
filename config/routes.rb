7a55c688bf10f44faea551c08b0d2fa9c9952aae
Denise <meagher.denise@gmail.com>
2016-07-27 20:17:22 -0400

    added user controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root 'sessions#index'
   get 'sessions/new'
 
-  resources :profiles
+  resources :profiles do
+    resources :users
+  end
   resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
