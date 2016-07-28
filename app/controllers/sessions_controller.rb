83cc269b0b80a06e89a7ad77b93f0786b317dc42
Jeremy Anderson <jande081@fiu.edu>
2016-07-27 20:52:27 -0400

    added the sessions create method to update the sessions array to hold the users username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,26 @@
 class SessionsController < ApplicationController
-  def new
+ 
+  def index
+  	@profile = Profile.new
+  	render 'index'
   end
+
+  def new
+  	# @profile = Profile.new
+  end
+
+  def create 
+  	@user = Profiles.
+		find_by(username: params[:username]).
+			try(:authenticate, params[:password])
+
+		return render action: 'new' unless @user
+
+		# logged in, hooray
+
+		session[:user_id] = @user.id
+
+		redirect_to notes_path
+
+	end
 end
