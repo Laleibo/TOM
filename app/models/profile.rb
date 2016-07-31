b61557315fcfd7ba3e55e8a937800151f8e57cf9
Denise <meagher.denise@gmail.com>
2016-07-31 13:18:27 -0400

    added email and password validation for profile setup

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,6 +4,9 @@ class Profile < ApplicationRecord
   has_many :orders, through: :users
   has_many :products, through: :orders
 
+validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, uniqueness: true, confirmation: true
+validates :password, confirmation: true, length: { minimum: 8 }
+
 # def process_payment
 # customer = Stripe::Customer.create email: email,
 #                                        card: card_token
