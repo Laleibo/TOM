30b41732cad9402616b9890aec30f3cd074d34b1
Laleibo <Laleibo@gmail.com>
2016-08-01 09:16:45 -0400

    Profile Hold/activate and Cancel features

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -8,16 +8,4 @@ class Profile < ApplicationRecord
 validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, uniqueness: true, confirmation: true
 # validates :password, confirmation: true, length: { minimum: 8 }, :if => :password
 
-
-# def process_payment
-# customer = Stripe::Customer.create email: email,
-#                                        card: card_token
-#
-#     Stripe::Charge.create customer: customer.id,
-#                           amount: product.price * 100,
-#                           description: product.name,
-#                           currency: 'usd'
-
-
-
 end
